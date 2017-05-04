6ec2e35a4e4298be658f0c323a933fce98cd1552
tdh62 <821028463@qq.com>
2017-05-04 10:18:03 +0800

    Update fooster.php

--- system/fooster.php
+++ system/fooster.php
@@ -10,5 +10,5 @@
 ?>
 <?php
 	echo '<hr /><center><font size="-1",color="gray"><p>毛概课程问卷——就业问题</p></font></center>';
-    echo '<center><font size="-2" color ="gray"><small>联系方式：tdh6@qq.com|<a href="http://www.miitbeian.gov.cn/">滇ICP备17001015号-1</a>|<a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=53010202000217" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="http://www.tdh6.top/ba.png" height="10" width="10"/>滇公网安备 53010202000217号</small></font></center>';
+   
 ?>
